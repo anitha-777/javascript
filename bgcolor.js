b296c2e51e37468f8f05a1715623ecd6ae0b7444
anitha-777 <166722328+anitha-777@users.noreply.github.com>
2025-09-02 19:57:38 +0530

    Add files via upload

---
+++ bgcolor.js
@@ -0,0 +1,20 @@
+
+let redbtn=document.getElementById("redbtn");
+let greenbtn=document.getElementById("greenbtn");
+let bluebtn=document.getElementById("bluebtn");
+function color_red(){
+    document.body.style.backgroundColor="red";
+    redbtn.style.backgroundColor="red";
+}
+
+function color_lightblue(){
+    document.body.style.backgroundColor="lightblue";
+    greenbtn.style.backgroundColor="lightblue";
+}
+
+function color_palevioletred(){
+    document.body.style.backgroundColor="palevioletred";
+    bluebtn.style.backgroundColor="palevioletred";
+}
+
+ 
